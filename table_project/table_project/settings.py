412f2cc0f7d1f7f834779a2cc16cee25bcba19bc
Nick Mol <nickmol96@hotmail.com>
2022-02-05 19:21:50 +0100

    a change

--- table_project/table_project/settings.py
+++ table_project/table_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7ajl1k0xzm7+nt0ze5e0w-1l9f&!ee)+3=yf!@&)ro1=k5c=j='
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production! 
 DEBUG = True
 
 ALLOWED_HOSTS = []
