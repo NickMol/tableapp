30c550c3c2e53d435b30f1e52ccc70be368083f2
Nick Mol <nickmol96@hotmail.com>
2022-02-05 19:31:57 +0100

    a change

--- table_project/table/models.py
+++ table_project/table/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class TableData(models.Model):
+    name = models.CharField(max_length=200,blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.naam
